0946245d4291285af0a1de0c5fadf5da28e47f8f
yashwardhan61 <yashrock.ys61@gmail.com>
2025-12-03 22:50:06 +0530

    Day 3 Potd done

---
+++ Day3/A_Theatre_Square.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+public class A_Theatre_Square {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextInt();
+        long m = sc.nextInt();
+        long a = sc.nextInt();
+        long k=(n+a-1)/a;
+        long l=(m+a-1)/a;
+        long result=k*l;
+        System.out.println(result);
+        sc.close();
+}
+}//time Complxcity=O(1)
