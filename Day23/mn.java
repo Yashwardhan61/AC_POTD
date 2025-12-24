9ad1401ff77a3f42ff8b9c5ad421c37743b37dba
yashwardhan61 <yashrock.ys61@gmail.com>
2025-12-24 23:38:25 +0530

    day23 done

---
+++ Day23/mn.java
@@ -0,0 +1,21 @@
+package Day23;
+public class mn {
+    public int[] minOperations(String boxes) {
+        int n = boxes.length();
+        int[] answer = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int operations = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (boxes.charAt(j) == '1') {
+                    operations += Math.abs(i - j);
+                }
+            }
+
+            answer[i] = operations;
+        }
+
+        return answer;
+    }
+}//TC=(n*n)
